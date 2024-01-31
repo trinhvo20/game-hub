6145faad1218118929f64b30b611c52e746c852a
Trinh Vo <62305589+trinhvo20@users.noreply.github.com>
2024-01-31 17:49:48 -0600

    Build the Navigation Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,29 @@
 import { useState } from "react";
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
     return (
-        <>
-            <Grid
-                templateAreas={{
-                    base: `"nav" "main"`,
-                    lg: `"nav nav" "aside main"`, //1000px
-                }}
-            >
-                <GridItem area="nav" bg="coral">
-                    Nav
-                </GridItem>
+        <Grid
+            templateAreas={{
+                base: `"nav" "main"`,
+                lg: `"nav nav" "aside main"`, //1000px
+            }}
+        >
+            <GridItem area="nav">
+                <NavBar />
+            </GridItem>
 
-                <Show above="lg">
-                    <GridItem area="aside" bg="gold">
-                        Aside
-                    </GridItem>
-                </Show>
-
-                <GridItem area="main" bg="dodgerblue">
-                    Main
+            <Show above="lg">
+                <GridItem area="aside" bg="gold">
+                    Aside
                 </GridItem>
-            </Grid>
-        </>
+            </Show>
+
+            <GridItem area="main" bg="dodgerblue">
+                Main
+            </GridItem>
+        </Grid>
     );
 }
 
