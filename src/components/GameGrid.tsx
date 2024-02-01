72a60299108b89d349b862b373a12bbf98d2aa3f
Trinh Vo <62305589+trinhvo20@users.noreply.github.com>
2024-01-31 20:52:05 -0600

    Display the Genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,7 +15,7 @@ const GameGrid = () => {
 
             <SimpleGrid
                 columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-                spacing={10}
+                spacing={5}
                 padding={10}
             >
                 {isLoading &&
