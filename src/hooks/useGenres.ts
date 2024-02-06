60fa097a62c5fbd6da5aba14cb834953a753d242
Trinh Vo <62305589+trinhvo20@users.noreply.github.com>
2024-02-06 17:46:14 -0600

    Ship Static Data (Genres)

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,5 @@
 import useData from "./useData";
+import geners from '../data/genres'
 
 export interface Genre {
     id: number;
@@ -6,6 +7,7 @@ export interface Genre {
     image_background: string;
 }
 
-const useGenres = () => useData<Genre>('/genres');
+// const useGenres = () => useData<Genre>('/genres'); // to get Genre data from backend server
+const useGenres = () => ({data: geners, isLoading: false, error: null});
 
 export default useGenres;
