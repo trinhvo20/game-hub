7d2194f1e9b427ee2f1c15e9a94feeb7be073551
Trinh Vo <62305589+trinhvo20@users.noreply.github.com>
2024-02-06 17:52:22 -0600

    Design GameGrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,29 +14,29 @@ const GameGrid = ({ gameQuery }: Props) => {
 
     const skeletons = [1, 2, 3, 4, 5, 6];
 
+    if (error) {
+        return <Text>{error}</Text>;
+    }
+
     return (
-        <>
-            {error && <Text>{error}</Text>}
-
-            <SimpleGrid
-                columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-                spacing={5}
-                paddingX={5}
-            >
-                {isLoading &&
-                    skeletons.map((skeleton) => (
-                        <GameCardContainer key={skeleton}>
-                            <GameCardSkeleton />
-                        </GameCardContainer>
-                    ))}
-
-                {games.map((game) => (
-                    <GameCardContainer key={game.id}>
-                        <GameCard game={game} />
+        <SimpleGrid
+            columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+            spacing={5}
+            paddingX={5}
+        >
+            {isLoading &&
+                skeletons.map((skeleton) => (
+                    <GameCardContainer key={skeleton}>
+                        <GameCardSkeleton />
                     </GameCardContainer>
                 ))}
-            </SimpleGrid>
-        </>
+
+            {games.map((game) => (
+                <GameCardContainer key={game.id}>
+                    <GameCard game={game} />
+                </GameCardContainer>
+            ))}
+        </SimpleGrid>
     );
 };
 
